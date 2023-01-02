21aa4f3c6c3951ecc3a944d056f28a6814256487
Nacho <ignaciocastroplanas@gmail.com>
2023-01-02 16:17:22 -0300

    mock de tareas e interface

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { task } from "./task"
+
+export const TASKS: task[] =[
+    {
+        id: 1,
+        text: "Terminar primer modulo",
+        day: "Agosto 5 12:00hs",
+        reminder:true
+    },
+    {
+        id: 2,
+        text: "Terminar segundo modulo",
+        day: "Agosto 5 16:00hs",
+        reminder:true
+    },
+    {
+        id: 3,
+        text: "Hacer la comida",
+        day: "Agosto 5 20:00hs",
+        reminder:false
+    },
+    {
+        id: 4,
+        text: "Leer libros",
+        day: "Agosto 5 22:00hs",
+        reminder:false
+    }
+]
---
+++ src/app/task.ts
@@ -0,0 +1,6 @@
+export interface task{
+    id?: number //Cuando creamos podría no venir
+    text: string;
+    day:string;
+    reminder:boolean;
+}
