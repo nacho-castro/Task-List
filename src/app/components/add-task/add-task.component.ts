2fb441740fcf6f71b228e0fa8e8b33b7ac6d7fad
Nacho <ignaciocastroplanas@gmail.com>
2023-01-06 17:57:05 -0300

    form reset

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { UiService } from 'src/app/service/ui.service';
 import { task } from 'src/app/task';
@@ -8,31 +9,31 @@ import { task } from 'src/app/task';
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent implements OnInit{
+export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<task> = new EventEmitter();
-  text:string = "";
-  day:string = "";
-  reminder:boolean = false;
-  showAddTask:boolean = false;
+  text: string = "";
+  day: string = "";
+  reminder: boolean = false;
+  showAddTask: boolean = false;
   subscription?: Subscription;
 
-  constructor(private uiService : UiService){
+  constructor(private uiService: UiService) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
   }
 
   ngOnInit(): void {
-    
+
   }
 
-  onSubmit(){
-    if(this.text.length === 0){
+  onSubmit(login: NgForm) {
+    if (this.text.length === 0) {
       alert("Please add a task!");
       return
     }
-    const {text, day, reminder} = this
-    const newTask = {text, day, reminder}
+    const { text, day, reminder } = this
+    const newTask = { text, day, reminder }
 
     this.onAddTask.emit(newTask)
-    
+    login.reset()
   }
 }
